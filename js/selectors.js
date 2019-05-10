5912260f6b6ac372181bca2091fd8a3f4937333f
Nicolas Marini <nmarini44@gmail.com>
2019-05-10 19:19:01 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return 
+}
+
+function ninjaBabySelector(){
+  return 
+}
+
+function divSelector(){
+  return 
+}
+
+function firstListItem(){
+  return 
+}
